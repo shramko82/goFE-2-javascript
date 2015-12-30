a4f024b7bd945f57bff886e7666c4029652e81d6
Alex <shramko82@gmail.com>
2015-12-30 17:17:03 +0200

    lesson 7-8

---
+++ lesson7_8/js/tabs.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('.tabs li').on('click', function() {
+        $(this).addClass('active')
+            .siblings().removeClass('active');
+        $('.wrapper .article').removeClass('active')
+            .eq($(this).index()).addClass('active');
+    });
+});
---
+++ lesson7_8/js/tooltip.js
@@ -0,0 +1,15 @@
+$(function(){
+    $('input')
+        .on('mouseover', function () {
+            $(this).siblings('.tooltip').addClass('show');
+        })
+        .on('mouseout', function () {
+            $(this).siblings('.tooltip').removeClass('show');
+        });
+    $('.button')
+        .on('click', function (e) {
+            e.preventDefault();
+            $('.tooltip').addClass('show');
+        });
+});
+
